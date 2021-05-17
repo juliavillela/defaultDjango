b7b836e3665e5ac5099ecccc3aaa433abe271eeb
juliavillela <33360385+juliavillela@users.noreply.github.com>
2021-05-17 09:22:26 -0300

    adds dismiss functionality to alert

--- base/static/base/js/components/message.js
+++ base/static/base/js/components/message.js
@@ -1,14 +1,22 @@
-const message_container = document.querySelector("#django-message-container")
+const message_container = document.querySelector("#django-message-container");
+const message_container_close = document.querySelector("#close-django-message");
 
-
+//if messages, displays message container for 6000ms
+// user can dismiss message by clicking the close button
 export default function handleMessageContainer(){
-    if (message_container.childElementCount>0){
+    if (message_container.childElementCount>1){
         console.log("message has children")
+        message_container_close.addEventListener('click', () => close(message_container) )
         message_container.style.marginTop = 0;
-        const timeout = 1500;
+        const timeout = 6000;
         setTimeout(() => {
-            message_container.style.marginTop = '-4em';
+            message_container.style.marginTop = '-10em';
         }, timeout);
     }
     console.log("no messages")
+}
+
+
+function close(element, margin_top='-10em'){
+    element.style.marginTop = '-10em';
 }
