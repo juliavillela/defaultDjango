1ad2fda749cb5908d6c85f9f05bfc0502fd45fa5
juliavillela <33360385+juliavillela@users.noreply.github.com>
2021-06-13 15:16:16 -0300

    create viewset for Dummy model

--- api/views.py
+++ api/views.py
@@ -1,5 +1,26 @@
+
+from django.db.models import query
 from django.shortcuts import render
+
+from rest_framework import viewsets
+from rest_framework.response import Response
+
+from .serializers import DummySerializer, PartialDummySerializer
+from .models import Dummy
+from api import serializers
 
 # Create your views here.
 def index(request):
     return render(request, 'api/index.html')
+
+def browse(request):
+    return render(request, 'api/browse.html')
+
+class DummyViewSet(viewsets.ModelViewSet):
+    queryset = Dummy.objects.all()
+    serializer_class = DummySerializer
+
+    def list(self, request):
+        queryset = Dummy.objects.filter(public=True)
+        serializer = PartialDummySerializer(queryset, many=True)
+        return Response(serializer.data)
