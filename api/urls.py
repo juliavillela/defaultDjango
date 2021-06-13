c72bc06fde34f4a86950a963504fcd9699fbd87c
juliavillela <33360385+juliavillela@users.noreply.github.com>
2021-06-13 15:18:02 -0300

    register viewset fro dummy to urls

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from rest_framework import routers
+
 from . import views
 
 # base App urls
@@ -7,3 +9,8 @@ app_name= 'api'
 urlpatterns = [
     path('', views.index, name='api_index'),
 ]
+
+router = routers.SimpleRouter(True)
+router.register(r'dummy', views.DummyViewSet)
+
+urlpatterns += router.urls
